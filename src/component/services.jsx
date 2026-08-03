0ff7044d1464d79b75d7bbf78e322dc997d3d6c6
Alden Christian <105768086+Irukandjie@users.noreply.github.com>
2026-08-03 08:05:25 +0700

    Update service title and description in services.jsx

--- src/component/services.jsx
+++ src/component/services.jsx
@@ -24,8 +24,8 @@ const ServicesSection = () => {
 
   const services = [
     {
-      id: 1, title: "SIPU",
-      desc: "Akses Informasi Infrastruktur Kabupaten Purwodadi Melalui Peta Interaktif.",
+      id: 1, title: "SINTARAMA",
+      desc: "Sistem Informasi Tata Ruang Masyarakat",
       icon: <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" /></svg>,
       themeColor: "text-amber-500", themeBorder: "border-amber-500", themeBgLight: "bg-amber-50", themeBgHover: "group-hover/btn:bg-amber-500", delay: "delay-100"
     },
